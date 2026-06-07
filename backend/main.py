23e758cef16dc398eeaf673225337c97ccdd964c
frazyusaf <frazyusaf@gmail.com>
2026-06-07 15:00:46 +0500

    fix: open CORS for all origins

--- backend/main.py
+++ backend/main.py
@@ -24,8 +24,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("*")],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
